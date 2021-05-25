c24dae3c5c5cf39e6abfc8798b4f8dce6287c66e
Nelfdesign <fabrice.design85@gmail.com>
2021-05-25 10:01:26 +0200

    add driver infos in drawer

--- app/src/main/java/fr/nelfdesign/kotlinuberclone/Common.kt
+++ app/src/main/java/fr/nelfdesign/kotlinuberclone/Common.kt
@@ -1,14 +1,24 @@
 package fr.nelfdesign.kotlinuberclone
 
 import fr.nelfdesign.kotlinuberclone.model.DriverInfoModel
+import java.lang.StringBuilder
 
 /**
  * Created by fabricedesign at 5/21/21
  * fr.nelfdesign.kotlinuberclone
  */
 object Common {
+    fun buildWelcomeMessage(): String {
+        return StringBuilder("Welcome, ")
+            .append(currentUser!!.firstName)
+            .append(" ")
+            .append(currentUser!!.lastName)
+            .toString()
+    }
 
+    //Database name
     val DRIVERS_LOCATION_REFERENCE: String = "DriversLocation"
-    var currentUser: DriverInfoModel? = null
     val DRIVER_INFO_REFERENCE: String = "DriverInfo"
+
+    var currentUser: DriverInfoModel? = null
 }
--- app/src/main/java/fr/nelfdesign/kotlinuberclone/DriverHomeActivity.kt
+++ app/src/main/java/fr/nelfdesign/kotlinuberclone/DriverHomeActivity.kt
@@ -1,7 +1,12 @@
 package fr.nelfdesign.kotlinuberclone
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -13,10 +18,17 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.nav_header_main.*
 
 class DriverHomeActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navView: NavigationView
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,18 +36,58 @@ class DriverHomeActivity : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
+        navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home,
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        init(context = this@DriverHomeActivity)
+    }
+
+    private fun init(context: Context){
+        navView.setNavigationItemSelectedListener { it ->
+            if(it.itemId == R.id.nav_side_out){
+                val builder = AlertDialog.Builder(this@DriverHomeActivity)
+                builder.setTitle("")
+                    .setMessage("Do you really want to sign out?")
+                    .setNegativeButton("CANCEL") { dialogInterface, _ -> dialogInterface.dismiss() }
+                    .setPositiveButton("OK") { _, _ ->
+                        FirebaseAuth.getInstance().signOut()
+                        val intent = Intent(this@DriverHomeActivity, SplashScreenActivity::class.java)
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(intent)
+                        finish()
+                    }
+                    .setCancelable(false)
+
+                val dialog = builder.create()
+                dialog.setOnShowListener {
+                    dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+                        .setTextColor(ContextCompat.getColor(context, android.R.color.holo_red_dark))
+                    dialog.getButton(AlertDialog.BUTTON_NEGATIVE)
+                        .setTextColor(ContextCompat.getColor(context, R.color.colorAccent))
+                }
+                dialog.show()
+            }
+            true
+        }
+
+        val headerView = navView.getHeaderView(0)
+        val txtName = headerView.findViewById<View>(R.id.txt_name) as TextView
+        val txtPhone = headerView.findViewById<View>(R.id.txt_phone) as TextView
+        val txtStar = headerView.findViewById<View>(R.id.txt_star) as TextView
+
+        txtName.text = Common.buildWelcomeMessage()
+        txtPhone.text = Common.currentUser!!.phoneNumber
+        txtStar.text = Common.currentUser!!.rating.toString()
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
